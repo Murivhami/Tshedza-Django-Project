5aeb3103c90e58ba16ab2b0255cb7a1cf9c3150a
Tshedza <tshedzamurivhami@gmail.com>
2025-03-26 19:13:31 +0200

    Created serializers for models

--- Calorie_Tracker/urls.py
+++ Calorie_Tracker/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from FitFair.views import MealViewSet, MealTypeViewSet, NutritionalProductViewSet
+
+router = DefaultRouter()
+router.register(r'meals', MealViewSet)
+router.register(r'meal-type', MealTypeViewSet)
+router.register(r'nutritional_product', NutritionalProductViewSet)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('FitFair/', include(router.urls)),
 ]
--- FitFair/views.py
+++ FitFair/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from .models import CustomUser, MealType, Meal, NutritionalProduct
+from rest_framework import viewsets
+from .serializers import CustomUserSerializer, MealSerializer, MealTypeSerializer, NutritionalProductSerializer
+
+class MealViewSet(viewsets.ModelViewSet):
+    queryset = Meal.objects.all()
+    serializer_class = MealSerializer
+
+class MealTypeViewSet(viewsets.ModelViewSet):
+    queryset = MealType.objects.all()
+    serializer_class = MealTypeSerializer
+
+class NutritionalProductViewSet(viewsets.ModelViewSet):
+    queryset = NutritionalProduct.objects.all()
+    serializer_class = NutritionalProductSerializer
+
+
+
 
 # Create your views here.
