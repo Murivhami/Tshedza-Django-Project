01c383fbe65bace7c5390e075a3ff085005b75b2
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-04-03 22:18:24 +0200

    Update forms.py

--- FitFair/forms.py
+++ FitFair/forms.py
@@ -7,12 +7,13 @@ from .models import CustomUser
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
-
+#Allows user to register by filling in the fields.
     class Meta:
         model = CustomUser
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']  
 
 class MealForm(forms.ModelForm):
+#Allows users to log their meals and saves info into the database.
     class Meta:
         model = Meal
         fields = ['meal_of_the_day', 'food_item','fats', 'proteins', 'carbs' ,'quantity'] #'total_calories']
